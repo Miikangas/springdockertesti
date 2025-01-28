07929382644c680f95eec6151920e8aa4c27167d
OpiskelijaMiika <miika.kangas@seamk.fi>
2025-01-28 12:30:25 +0200

    lisätty spring alustus

---
+++ harjoitus/src/main/java/com/ohtuotanto2/harjoitus/HarjoitusApplication.java
@@ -0,0 +1,13 @@
+package com.ohtuotanto2.harjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarjoitusApplication.class, args);
+	}
+
+}
---
+++ harjoitus/src/test/java/com/ohtuotanto2/harjoitus/HarjoitusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohtuotanto2.harjoitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarjoitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
